e6ae72239d250d4e80aef37beb5b9273106ec9a6
EC2 Default User <ec2-user@ip-172-31-32-253.ap-northeast-1.compute.internal>
2020-05-26 08:20:29 +0000

    Finish user signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,12 +4,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   
   def setup
     @base_title = "Tabetter"
+    @user = users(:user1)
   end
   
+  test "should get show" do
+    get user_path(@user)
+    assert_response :success
+    assert_select "title", "#{@user.name} | #{@base_title}"
+    assert_select "h2", @user.name
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
-    assert_select "title", "Sign up | #{@base_title}"
+    assert_select "title", "登録 | #{@base_title}"
   end
 
 end
