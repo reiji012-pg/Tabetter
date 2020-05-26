28ca798e75a9f01aaeeaad386e116e9b234bb768
EC2 Default User <ec2-user@ip-172-31-32-253.ap-northeast-1.compute.internal>
2020-05-26 01:30:07 +0000

    Add static pages and User controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Tabetter"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  
+  root 'static_pages#home'
+  get "/about", to: "static_pages#about"
+  get "/signup", to: "users#new"
+
 end
