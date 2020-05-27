876f89e6be1c83ad048f52010ebbaa78fbc2b1db
EC2 Default User <ec2-user@ip-172-31-32-253.ap-northeast-1.compute.internal>
2020-05-27 10:02:14 +0000

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in(@user)
+      log_in @user
       flash[:success] = "アカウント登録が完了しました！"
       redirect_to @user
     else
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,17 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
   
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+  
+# 総合テスト用（総合テストではSessionを使えないため）
+class ActionDispatch::IntegrationTest
+  
+  def log_in_as(user, password: "password", remember_me: "1")
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
